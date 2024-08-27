8c743be1de9d97cb1ef0ec9296775808755d1589
Passionhruit <korea_stan@naver.com>
2024-08-27 11:22:27 +0900

    feat: object3d 및 group 움직임 단위 설정
    scene, group, mesh 설정해서 group 움직임 생성

--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -6,6 +6,7 @@ import * as THREE from "three";
 const ThreeElement = () => {
   const { size, gl, scene, camera } = useThree();
   const boxRef = useRef<THREE.Mesh>(null);
+  const groupRef = useRef<THREE.Group>(null);
 
   // const box = useControls({
   //   rotation: {
@@ -17,29 +18,68 @@ const ThreeElement = () => {
   // });
 
   useFrame((state, delta) => {
-    //   console.log("state:", state);
-    //   console.log("delta", delta);
-    // console.log("boxRef", boxRef);
     // boxRef.current.rotation.x += delta;
     // boxRef.current.rotation.y -= 0.01;
     // boxRef.current.scale.z += 0.01;
+    // scene.position.x += 0.01; // scene 자체가 x축으로 이동, object3d 이기 때문에 position 속성 사용 가능
+    groupRef.current.rotation.x += delta;
   });
   return (
     <>
       <directionalLight position={[5, 5, 5]} />
-      <mesh
-        ref={boxRef}
+      <group
+        ref={groupRef}
         position={[0, 0, 0]}
-        scale={[1, 1, 1]}
         rotation={[
           THREE.MathUtils.degToRad(0),
-          THREE.MathUtils.degToRad(0),
+          THREE.MathUtils.degToRad(45),
           THREE.MathUtils.degToRad(0),
         ]}
       >
-        <boxGeometry />
-        <meshStandardMaterial color="red" />
-      </mesh>
+        <mesh
+          ref={boxRef}
+          position={[0, 0, 0]}
+          // position-x={[5]} // position x 축만 이동하고 싶은 경우
+          scale={[1, 1, 1]}
+          rotation={[
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(0),
+          ]}
+        >
+          <boxGeometry />
+          <meshStandardMaterial color="red" />
+        </mesh>
+        <mesh
+          ref={boxRef}
+          position={[2, 0, 0]}
+          // position-x={[5]} // position x 축만 이동하고 싶은 경우
+          scale={[1, 1, 1]}
+          rotation={[
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(0),
+          ]}
+        >
+          <axesHelper args={[3]} />
+          <boxGeometry />
+          <meshStandardMaterial color="blue" />
+        </mesh>
+        <mesh
+          ref={boxRef}
+          position={[0, 2, 0]}
+          // position-x={[5]} // position x 축만 이동하고 싶은 경우
+          scale={[1, 1, 1]}
+          rotation={[
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(0),
+          ]}
+        >
+          <boxGeometry />
+          <meshStandardMaterial color="green" />
+        </mesh>
+      </group>
     </>
   );
 };
