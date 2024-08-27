04e5cc3308f9082f0aa310c856bf67067cec2316
Passionhruit <korea_stan@naver.com>
2024-08-27 12:00:18 +0900

    feat: R3F 에서 mesh 를 추가하는 방법
    1. mesh 컴포넌트 안에 geometry , material 감싸기
    2. mesh 에 속성으로 geometry 받아오고 material 감싸기
    3. drei 에서 원하는 geometry 받아오고 material 감싸기

--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -1,6 +1,7 @@
 import { useFrame, useThree } from "@react-three/fiber";
 import React, { useRef } from "react";
 import * as THREE from "three";
+import { Box } from "@react-three/drei";
 // import { useControls } from "leva";
 
 const ThreeElement = () => {
@@ -22,62 +23,33 @@ const ThreeElement = () => {
     // boxRef.current.rotation.y -= 0.01;
     // boxRef.current.scale.z += 0.01;
     // scene.position.x += 0.01; // scene 자체가 x축으로 이동, object3d 이기 때문에 position 속성 사용 가능
-    groupRef.current.rotation.x += delta;
   });
   return (
     <>
       <directionalLight position={[5, 5, 5]} />
-      <group
-        ref={groupRef}
-        position={[0, 0, 0]}
-        rotation={[
-          THREE.MathUtils.degToRad(0),
-          THREE.MathUtils.degToRad(45),
-          THREE.MathUtils.degToRad(0),
-        ]}
-      >
+      <group ref={groupRef} position={[0, 0, 0]}>
+        {/* mesh 추가하는 방법3, drei 이용, 제일 쉬운방식으로 구조에 익숙해지면 사용 추천*/}
+        <Box position={[-2, 0, 0]}>
+          <meshStandardMaterial color="green" />
+        </Box>
+        {/* mesh 추가하는 방법2 */}
+        <mesh geometry={new THREE.BoxGeometry(1, 1, 1, 1)}>
+          <meshStandardMaterial color="blue" />
+        </mesh>
+        {/* mesh 추가하는 방법 1 => mesh,geometry,material 의 관계가 제일 중요하므로 이방식 추천*/}
         <mesh
           ref={boxRef}
-          position={[0, 0, 0]}
+          position={[0, 0, 2]}
           // position-x={[5]} // position x 축만 이동하고 싶은 경우
           scale={[1, 1, 1]}
-          rotation={[
-            THREE.MathUtils.degToRad(0),
-            THREE.MathUtils.degToRad(0),
-            THREE.MathUtils.degToRad(0),
-          ]}
+          // rotation={[
+          //   THREE.MathUtils.degToRad(0),
+          //   THREE.MathUtils.degToRad(0),
+          //   THREE.MathUtils.degToRad(0),
+          // ]}
         >
           <boxGeometry />
           <meshStandardMaterial color="red" />
-        </mesh>
-        <mesh
-          ref={boxRef}
-          position={[2, 0, 0]}
-          // position-x={[5]} // position x 축만 이동하고 싶은 경우
-          scale={[1, 1, 1]}
-          rotation={[
-            THREE.MathUtils.degToRad(0),
-            THREE.MathUtils.degToRad(0),
-            THREE.MathUtils.degToRad(0),
-          ]}
-        >
-          <axesHelper args={[3]} />
-          <boxGeometry />
-          <meshStandardMaterial color="blue" />
-        </mesh>
-        <mesh
-          ref={boxRef}
-          position={[0, 2, 0]}
-          // position-x={[5]} // position x 축만 이동하고 싶은 경우
-          scale={[1, 1, 1]}
-          rotation={[
-            THREE.MathUtils.degToRad(0),
-            THREE.MathUtils.degToRad(0),
-            THREE.MathUtils.degToRad(0),
-          ]}
-        >
-          <boxGeometry />
-          <meshStandardMaterial color="green" />
         </mesh>
       </group>
     </>
