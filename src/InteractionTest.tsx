edf0c73e62d3591c6f2994378e2127a062410a49
Passionhruit <korea_stan@naver.com>
2024-08-28 11:36:19 +0900

    feat: object click 이벤트 전달 막기, raycaster
    이벤트 버블링 막기

--- src/InteractionTest.tsx
+++ src/InteractionTest.tsx
@@ -3,36 +3,23 @@ import * as THREE from "three";
 
 const InteractionTest = () => {
   const clickFunc = (e: any) => {
+    // 한 축 방향에서 클릭하면 세개의 상자가 모두 바뀌게됨, 이벤트 버블링을 막아서 겹치는 부분이 생기더라도 해당되지 않게 함
+    e.stopPropagation();
     e.object.material.color = new THREE.Color("green");
-  };
-
-  const overFunc = (e: any) => {
-    e.object.scale.set(2, 2, 2);
-    e.object.scale.x = 3;
-  };
-
-  const outFunc = (e: any) => {
-    e.object.scale.set(1, 1, 1);
   };
   return (
     <>
       <ambientLight />
       <directionalLight intensity={5} />
-      <mesh
-        onClick={(e) => clickFunc(e)}
-        // onContextMenu={(e) => console.log("context menu")}
-        // onDoubleClick={(e) => console.log("double click")}
-        // onWheel={(e) => console.log("wheel spins")}
-        // onPointerUp={(e) => console.log("up")}
-        // onPointerDown={(e) => console.log("down")}
-        onPointerOver={(e) => overFunc(e)}
-        onPointerOut={(e) => outFunc(e)}
-        // onPointerEnter={(e) => console.log("enter")}
-        // onPointerLeave={(e) => console.log("leave")}
-        // onPointerMove={(e) => console.log("move")}
-        // onPointerMissed={() => console.log("missed")}
-        // onUpdate={(self) => console.log("props have been updated")}
-      >
+      <mesh onClick={(e) => clickFunc(e)} position={[-2, 0, 0]}>
+        <boxGeometry />
+        <meshStandardMaterial />
+      </mesh>
+      <mesh onClick={(e) => clickFunc(e)} position={[0, 0, 0]}>
+        <boxGeometry />
+        <meshStandardMaterial />
+      </mesh>
+      <mesh onClick={(e) => clickFunc(e)} position={[2, 0, 0]}>
         <boxGeometry />
         <meshStandardMaterial />
       </mesh>
